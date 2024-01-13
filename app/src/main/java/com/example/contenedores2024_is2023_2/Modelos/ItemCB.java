c588392c92f26b1cbffb3ad66be0faa1251adb2e
DELL <cristian_uteq@hotmail.com>
2024-01-12 22:43:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/contenedores2024_is2023_2/Modelos/ItemCB.java
@@ -0,0 +1,37 @@
+package com.example.contenedores2024_is2023_2.Modelos;
+
+import androidx.annotation.NonNull;
+
+public class ItemCB {
+
+    int ID;
+    String Descripcion;
+
+    @NonNull
+    @Override
+    public String toString() {       return Descripcion;    }
+
+    public ItemCB(int ID, String descripcion) {
+        this.ID = ID;
+        Descripcion = descripcion;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+
+
+}
